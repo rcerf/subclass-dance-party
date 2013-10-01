48669b7f16c9482755c8dbec49eca2fad792cb01
Rick Cerf <rickcerf@gmail.com>
2013-10-01 16:48:24 -0700

    working BlinkyDancer

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -4,10 +4,19 @@ var BlinkyDancer = function(top, left, timeBetweenSteps){
 };
 
 BlinkyDancer.prototype = Object.create(Dancer.prototype);
+
 BlinkyDancer.prototype.constructor = BlinkyDancer;
+
 BlinkyDancer.prototype.step = function(){
   Dancer.prototype.step.call(this);
   this.$node.toggle();
+  console.log("blinkyDancer Step");
+};
+
+BlinkyDancer.prototype.classChange = function(){
+  console.log("BlinkyDancer prototype");
+  Dancer.prototype.classChange.call(this);
+  this.$node.toggleClass("coloredDancer");
 };
 
 
